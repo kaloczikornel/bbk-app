9fb4aadbd19d39b86ce77c8259837d0b8f9a5b93
Kálóczi Kornél <kaloczi.kornel.done@gmail.com>
2022-04-29 22:22:18 +0200

    objRepo db done

--- route/index.js
+++ route/index.js
@@ -26,81 +26,92 @@ const getUserApplies = require("../middlewares/applicant/getUserApplies");
 
 const render = require("../middlewares/render");
 
+const UserModel = require('../models/user');
+const BlogModel = require('../models/blog');
+const EventModel = require('../models/event');
+const ApplicantModel = require('../models/applicant');
+
+
 module.exports = function (app) {
-  const objRepo = {};
+    const objRepo = {
+        UserModel: UserModel,
+        BlogModel: BlogModel,
+        EventModel: EventModel,
+        ApplicantModel: ApplicantModel
+    };
 
-  app.use(
-    "/reg",
-    inverseAuth(objRepo),
-    uniqueEmail(objRepo),
-    uniqueNick(objRepo),
-    saveUser(objRepo),
-    render(objRepo,"registration")
-  );
-  app.use(
-    "/login",
-    inverseAuth(objRepo),
-    loginDataCheck(objRepo),
-    render(objRepo,"login")
-  );
-  app.use(
-    "/forgotten",
-    inverseAuth(objRepo),
-    sendForgotten(objRepo),
-    render(objRepo,"forgotten")
-  );
-  app.get("/logout", auth(objRepo), logout(objRepo));
+    app.use(
+        "/reg",
+        inverseAuth(objRepo),
+        uniqueEmail(objRepo),
+        uniqueNick(objRepo),
+        saveUser(objRepo),
+        render(objRepo, "registration")
+    );
+    app.use(
+        "/login",
+        inverseAuth(objRepo),
+        loginDataCheck(objRepo),
+        render(objRepo, "login")
+    );
+    app.use(
+        "/forgotten",
+        inverseAuth(objRepo),
+        sendForgotten(objRepo),
+        render(objRepo, "forgotten")
+    );
+    app.get("/logout", auth(objRepo), logout(objRepo));
 
-  app.use(
-    "/event/:eventid/apply",
-    auth(objRepo),
-    getEventByID(objRepo),
-    saveApplicant(objRepo),
-    render(objRepo,"apply")
-  );
-  app.get("/events", getEvents(objRepo), render(objRepo,"events"));
-  app.use(
-    "event/:eventid/applicants",
-    auth(objRepo),
-    getApplicants(objRepo),
-    saveApplicant(objRepo),
-    render(objRepo,"applicants")
-  );
-  app.use("/newevent", auth(objRepo), saveEvent(objRepo), render(objRepo,"newEvent"));
-  app.get(
-    "/delevent/:eventid",
-    auth(objRepo),
-    getEvents(objRepo),
-    getApplicants(objRepo),
-    delApplicants(objRepo),
-    delEvent(objRepo)
-  );
+    app.use(
+        "/event/:eventid/apply",
+        auth(objRepo),
+        getEventByID(objRepo),
+        saveApplicant(objRepo),
+        render(objRepo, "apply")
+    );
+    app.get("/events", getEvents(objRepo), render(objRepo, "events"));
+    app.use(
+        "event/:eventid/applicants",
+        auth(objRepo),
+        getApplicants(objRepo),
+        saveApplicant(objRepo),
+        render(objRepo, "applicants")
+    );
+    app.use("/newevent", auth(objRepo), saveEvent(objRepo), render(objRepo, "newEvent"));
+    app.get(
+        "/delevent/:eventid",
+        auth(objRepo),
+        getEvents(objRepo),
+        getApplicants(objRepo),
+        delApplicants(objRepo),
+        delEvent(objRepo)
+    );
 
-  app.get("/blog", getBlogposts(objRepo), render(objRepo,"blog"));
-  app.get(
-    "/delpost/:blogpostid",
-    auth(objRepo),
-    getBlogposts(objRepo),
-    delPost(objRepo)
-  );
-  app.use("/newblog", auth(objRepo), savePost(objRepo), render(objRepo,"newBlog"));
+    app.get("/blog", getBlogposts(objRepo), render(objRepo, "blog"));
+    app.get(
+        "/delpost/:blogpostid",
+        auth(objRepo),
+        getBlogposts(objRepo),
+        delPost(objRepo)
+    );
+    app.use("/newblog", auth(objRepo), savePost(objRepo), render(objRepo, "newBlog"));
 
-  app.use(
-    "/profile",
-    auth(objRepo),
-    getUserInfo(objRepo),
-    getUserApplies(objRepo),
-    saveUser(objRepo),
-    saveApplicant(objRepo),
-    render(objRepo,"profile")
-  );
-  app.use(
-    "/users",
-    auth(objRepo),
-    getUsers(objRepo),
-    saveUser(objRepo),
-    render(objRepo,"users")
-  );
+    app.use(
+        "/profile",
+        auth(objRepo),
+        getUserInfo(objRepo),
+        getUserApplies(objRepo),
+        saveUser(objRepo),
+        saveApplicant(objRepo),
+        render(objRepo, "profile")
+    );
+    app.use(
+        "/users",
+        auth(objRepo),
+        getUsers(objRepo),
+        saveUser(objRepo),
+        render(objRepo, "users")
+    );
 
-  app.get('/', getEvents(objRepo), getBlogposts(objRepo), render(objRepo,'home'));
+    app.get('/', getEvents(objRepo), getBlogposts(objRepo), render(objRepo, 'home'));
 };
