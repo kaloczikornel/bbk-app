4df33a181f5ac57d3e79d0faa87e558c7d1657a9
Kálóczi Kornél <kaloczi.kornel.done@gmail.com>
2022-05-08 11:09:28 +0200

    users list is done

--- middlewares/user/getUsers.js
+++ middlewares/user/getUsers.js
@@ -1,7 +1,15 @@
-const requireOption = require('../requireOption');
+const requireOption = require("../requireOption");
 
 module.exports = function (objectrepository) {
-    return function (req, res, next) {
-        next();
+    const UserModel = requireOption(objectrepository, "UserModel");
+    return (req, res, next) => {
+        UserModel.find({}, (err, users) => {
+                if (err) {
+                    return next(err);
+                }
+                res.locals.users = users;
+                return next();
+            }
+        );
     };
 };
