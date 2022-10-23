ffc92058f68a9d4a703c583ac355ab5c6e2d53cf
Kálóczi Kornél <kornel580@gmail.com>
2022-10-23 17:29:00 +0200

    add providers
    auth0 provider, theme, and authContext added

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './hooks/useAuth';
+import constructTheme from './theme';
+
+const theme = constructTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <Auth0Provider
+                domain="dev-1ckx0xgt.us.auth0.com"
+                clientId="p6AgBJyX7B3k7I8D2wLeNjo7PAm7m2lC"
+                redirectUri={window.location.origin}
+            >
+                <AuthContextProvider>
+                    <ThemeProvider theme={theme}>
+                        <App />
+                    </ThemeProvider>
+                </AuthContextProvider>
+            </Auth0Provider>
         </BrowserRouter>
     </React.StrictMode>
 );
