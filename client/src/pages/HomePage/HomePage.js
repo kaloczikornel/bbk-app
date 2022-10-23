69de578aefd3cc7b7b8cba63f7ebdd1b1188b606
Kálóczi Kornél <kornel580@gmail.com>
2022-10-23 17:32:15 +0200

    add TODO

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
 
+// TODO sum up page, what we are, what do we do, maybe who we are...
 export const HomePage = () => {
     return (
-        <>
-            <Carousel animation="zoom" sx={{ marginTop: 10 }}>
-                <div style={{ background: 'red', width: 1152, height: 500 }}></div>
-                <div style={{ background: 'blue', width: '100%', height: '100%' }}></div>
-                <div style={{ background: 'green', width: '100%', height: '100%' }}></div>
-                <div style={{ background: 'yellow', width: '100%', height: '100%' }}></div>
-                <div style={{ background: 'purple', width: '100%', height: '100%' }}></div>
-            </Carousel>
-        </>
+        <Carousel animation="zoom" sx={{ marginTop: 10 }}>
+            <div style={{ background: 'red', width: 1152, height: 500 }} />
+            <div style={{ background: 'blue', width: '100%', height: '100%' }} />
+            <div style={{ background: 'green', width: '100%', height: '100%' }} />
+            <div style={{ background: 'yellow', width: '100%', height: '100%' }} />
+            <div style={{ background: 'purple', width: '100%', height: '100%' }} />
+        </Carousel>
     );
 };
 
