921e1100d9de232c615821afcd2ecf46d3da2425
Kálóczi Kornél <kornel580@gmail.com>
2022-10-23 17:25:02 +0200

    add routing based on user is authenticated or not

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,40 @@ import NavBarComponent from './components/NavBarComponent';
 import EventsPage from './pages/Events/EventsPage';
 import BlogPage from './pages/BlogPage/BlogPage';
 import HomePage from './pages/HomePage/HomePage';
+import { useAuth } from './hooks/useAuth';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 function App() {
+    const { isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) {
+        return (
+            <>
+                <NavBarComponent />
+                <Container maxWidth="lg" sx={{ marginTop: 15 }}>
+                    <Routes>
+                        <Route path="/" exact element={<HomePage />} />
+                        <Route path="/login" exact element={<Login />} />
+                        <Route path="/registration" exact element={<Login />} />
+                        <Route path="/events" exact element={<EventsPage />} />
+                        <Route path="/blog" exact element={<BlogPage />} />
+                    </Routes>
+                </Container>
+            </>
+        );
+    }
     return (
         <>
             <NavBarComponent />
             <Container maxWidth="lg" sx={{ marginTop: 15 }}>
                 <Routes>
-                    <Route path="/" exact element={<Login />} />
-                    <Route path="/home" exact element={<HomePage />} />
-                    <Route path="/registration" exact element={<Login />} />
-                    <Route path="/login" exact element={<Login />} />
+                    <Route path="/" exact element={<HomePage />} />
                     <Route path="/logout" exact element={<Login />} />
                     <Route path="/event/:eventid/apply" exact element={<Login />} />
                     <Route path="/events" exact element={<EventsPage />} />
                     <Route path="/event/:eventid/applicants" exact element={<Login />} />
                     <Route path="/blog" exact element={<BlogPage />} />
-                    <Route path="/me" exact element={<Login />} />
+                    <Route path="/me" exact element={<ProfilePage />} />
                     <Route path="/users" exact element={<Login />} />
                 </Routes>
             </Container>
