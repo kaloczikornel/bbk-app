8e7652339d55c3d87dd9a64102774450e929464e
Kálóczi Kornél <kornel580@gmail.com>
2022-10-23 17:32:04 +0200

    make eventspage call the backend for data

--- client/src/pages/Events/EventsPage.js
+++ client/src/pages/Events/EventsPage.js
@@ -1,79 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Grid, LinearProgress, Typography } from '@mui/material';
 import PagePaper from '../../components/PagePaper';
 import MediaCard from './components/MediaCard';
-import { Grid } from '@mui/material';
-const MOCK_EVENTS = [
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-    {
-        name: 'Event Name',
-        place: 'hely',
-        date: new Date(),
-        description: 'ez egy hosszú leírás ami arról szól, hogy milyen fasza ez a rendezvény',
-    },
-];
+import { AXIOS_METHOD, useApi } from '../../hooks/useApi';
 
 export const EventsPage = () => {
+    const [events, setEvents] = useState([]);
+    const [data, loading, error] = useApi(AXIOS_METHOD.GET, '/events?json');
+
+    useEffect(() => {
+        if (data) {
+            setEvents(data.events);
+        } else {
+            setEvents([]);
+        }
+    }, [data]);
     return (
         <PagePaper title="Események">
-            {MOCK_EVENTS.map((e) => {
-                return (
-                    <Grid item md={6}>
-                        <MediaCard
-                            date={e.date}
-                            description={e.description}
-                            place={e.place}
-                            name={e.name}
-                        ></MediaCard>
-                    </Grid>
-                );
-            })}
+            {loading && <LinearProgress />}
+            {error && <Typography variant="h5">Hupszi valami gond van...</Typography>}
+            {!loading &&
+                !error &&
+                (events.length === 0 ? (
+                    <Typography variant="h5" marginTop="40px">
+                        Sajnos jelenleg nincsen közelgő eseményünk! Gyere vissza később!
+                    </Typography>
+                ) : (
+                    events.map((e) => {
+                        return (
+                            <Grid item md={6}>
+                                <MediaCard
+                                    date={e.date}
+                                    description={e.description}
+                                    place={e.place}
+                                    name={e.name}
+                                />
+                            </Grid>
+                        );
+                    })
+                ))}
         </PagePaper>
     );
 };
