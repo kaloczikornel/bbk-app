2fef56bd2507160571d2052e93bd08f73a358353
Kálóczi Kornél <kornel580@gmail.com>
2022-10-31 17:07:21 +0100

    little rethink of useApi
    bring accestoken outside the hook and remove getAccessTokenWithPopup since it is useless

--- client/src/hooks/useApi.js
+++ client/src/hooks/useApi.js
@@ -2,37 +2,32 @@ import axios from 'axios';
 import { useCallback, useEffect, useState } from 'react';
 import { useAuth } from './useAuth';
 
-const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'http://localhost:4000';
 export const AXIOS_METHOD = {
     GET: 'GET',
     POST: 'POST',
     DELETE: 'DELETE',
+    PATCH: 'PATCH',
 };
+let accessToken = null;
 
-export function doApiCall(
-    method,
-    uri,
-    accessToken,
-    onSuccess,
-    onFailure = false,
-    data = undefined
-) {
-    axios({
-        method,
-        url: `${BASE_URL}${uri}`,
-        data,
-        headers: accessToken ? { authorization: `Bearer ${accessToken}` } : {},
-    })
-        .then((res) => {
-            onSuccess(res.data);
-        })
-        .catch((err) => {
-            console.error(err);
-            if (onFailure === false) {
-                return;
-            }
-            onFailure(err?.response?.data?.error, err);
+export async function doApiCall(method, uri, onSuccess, onFailure = false, data = undefined) {
+    try {
+        const res = await axios({
+            method,
+            url: `${BASE_URL}${uri}`,
+            data,
+            headers: accessToken ? { authorization: `Bearer ${accessToken}` } : {},
         });
+
+        onSuccess(res.data);
+    } catch (err) {
+        console.error(err);
+        if (onFailure === false) {
+            return;
+        }
+        onFailure(err?.response?.data?.error, err);
+    }
 }
 
 export function useApi(method, uri, postData = undefined, deps = []) {
@@ -40,37 +35,26 @@ export function useApi(method, uri, postData = undefined, deps = []) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const { getAccessTokenSilently, getAccessTokenWithPopup } = useAuth();
-    const [accessToken, setAccessToken] = useState(null);
 
     useEffect(() => {
         (async () => {
             try {
-                const token = await getAccessTokenSilently({
+                accessToken = await getAccessTokenSilently({
                     audience: 'https://bbk-app.hu',
                     scope: 'read:events',
                 });
-                setAccessToken(token);
             } catch (e) {
-                try {
-                    const token = await getAccessTokenWithPopup({
-                        audience: 'https://bbk-app.hu',
-                        scope: 'read:events',
-                    });
-                    setAccessToken(token);
-                } catch (err) {
-                    console.log(err);
-                }
+                console.log(e);
             }
         })();
-    }, [setAccessToken, getAccessTokenSilently, getAccessTokenWithPopup]);
+    }, [getAccessTokenSilently, getAccessTokenWithPopup]);
 
     const apiCallCallback = useCallback(
-        (apiPostData) => {
+        async (apiPostData) => {
             setLoading(true);
-            doApiCall(
+            await doApiCall(
                 method,
                 uri,
-                accessToken,
                 (responseData) => {
                     setData(responseData);
                     setError(false);
@@ -84,16 +68,7 @@ export function useApi(method, uri, postData = undefined, deps = []) {
                 apiPostData
             );
         },
-        [
-            method,
-            setData,
-            setError,
-            setLoading,
-            uri,
-            accessToken,
-            getAccessTokenSilently,
-            setAccessToken,
-        ]
+        [method, setData, setError, setLoading, uri, accessToken]
     );
 
     useEffect(() => {
