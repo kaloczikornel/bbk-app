72bec922e943ddbfd40617ce882de910ad570ebc
Kálóczi Kornél <kornel580@gmail.com>
2022-10-31 21:54:58 +0100

    add EventDetailsPage
    test route is for save the user in the DB so this is not the final version

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPage/Login';
 import NavBarComponent from './components/NavBarComponent';
-import EventsPage from './pages/Events/EventsPage';
+import EventsPage from './pages/EventsPage/EventsPage';
 import BlogPage from './pages/BlogPage/BlogPage';
 import HomePage from './pages/HomePage/HomePage';
 import { useAuth } from './hooks/useAuth';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import EventDetailsPage from './pages/EventsPage/EventDetailsPage';
 
 function App() {
     const { isAuthenticated } = useAuth();
@@ -33,10 +34,10 @@ function App() {
             <Container maxWidth="lg" sx={{ marginTop: 15 }}>
                 <Routes>
                     <Route path="/" exact element={<HomePage />} />
-                    <Route path="/logout" exact element={<Login />} />
+                    <Route path="/test" exact element={<Login />} />
                     <Route path="/event/:eventid/apply" exact element={<Login />} />
                     <Route path="/events" exact element={<EventsPage />} />
-                    <Route path="/event/:eventid/applicants" exact element={<Login />} />
+                    <Route path="/event/:eventId" exact element={<EventDetailsPage />} />
                     <Route path="/blog" exact element={<BlogPage />} />
                     <Route path="/me" exact element={<ProfilePage />} />
                     <Route path="/users" exact element={<Login />} />
