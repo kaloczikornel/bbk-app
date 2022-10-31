4c67a47968fc1f5060484467f38318537f40075e
Kálóczi Kornél <kornel580@gmail.com>
2022-10-31 17:09:56 +0100

    make a login flow for the backend
    the user has to saved in the db as well, so something is needed, this is not the final version

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,32 +1,66 @@
-import React from 'react';
-import { Button, Paper, Stack, Typography } from '@mui/material';
-import { useAuth } from '../../hooks/useAuth';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import { CircularProgress, Container } from '@mui/material';
+import axios from 'axios';
+import { BASE_URL } from '../../hooks/useApi';
 
 function Login() {
-    const { loginWithRedirect } = useAuth();
-    return (
-        <Paper
-            sx={{
-                position: 'fixed',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: 450,
-                height: 300,
-                padding: 5,
-            }}
-            elevation={12}
-        >
-            <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-                <Typography variant="h3" textAlign="center">
-                    Üdv a BBK oldalán
-                </Typography>
-                <Button fullWidth onClick={() => loginWithRedirect()}>
-                    Bejelentkezés
-                </Button>
-            </Stack>
-        </Paper>
-    );
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [success, setSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true);
+                const token = await getAccessTokenSilently({
+                    audience: 'https://bbk-app.hu',
+                    scope: 'read:events',
+                });
+                if (user && token) {
+                    const res = await axios.post(
+                        `${BASE_URL}/login`,
+                        {
+                            user,
+                        },
+                        {
+                            headers: {
+                                authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    setSuccess(res.data.success);
+                }
+            } catch (e) {
+                console.log(e);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, [getAccessTokenSilently, user]);
+
+    useEffect(() => {
+        if (success) {
+            navigate('/');
+        }
+    }, [navigate, success]);
+
+    if (loading) {
+        return (
+            <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+                <CircularProgress />
+            </Container>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return <div>You need to login first</div>;
+    }
+    return !loading && error && <div>Ajjaj baj van :(</div>;
 }
 
 export default Login;
